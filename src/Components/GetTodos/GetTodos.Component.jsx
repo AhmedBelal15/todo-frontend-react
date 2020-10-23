1809d858dac61f85a7cbd1383056598125bd7147
Your Name <ahmbelal95@gmail.com>
2020-10-23 19:33:54 +0200

    integrated with back end protected routes functionality + sending the token with every request

--- src/Components/GetTodos/GetTodos.Component.jsx
+++ src/Components/GetTodos/GetTodos.Component.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useEffect, useState } from "react";
 import Card from "../Card/Card.component";
 
-const GetTodos = ({ todo,setTodo }) => {
-  //Get user from localstorage
-  const userId = JSON.parse(localStorage.getItem("currentUser")).userObject._id;
+const GetTodos = ({ todo, setTodo }) => {
+  //Get token from localstorage
+  const token = JSON.parse(localStorage.getItem("currentUser")).accessToken;
   //Initialize State
   const [todos, setTodos] = useState([]);
   //Fetching Todos data
@@ -12,35 +12,37 @@ const GetTodos = ({ todo,setTodo }) => {
     //Fetch using IIFE
     (async function () {
       const response = await fetch("http://localhost:4000/api/user/gettodos", {
-        method: "post",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ userId }),
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          auth: `Bearer ${token}`,
+        },
       });
       const data = await response.json();
       setTodos(data);
     })();
   }, [todo]);
 
-  // Todo Handlers
-  const handleDelete = (id) => {
-    fetch(`http://localhost:4000/api/user/removeTodo/${id}`, {
-      method: 'delete',
-
-    })
-    setTodo({counter: todo.counter++})
+  // handleDelete
+  const handleDelete = async (id) => {
+   await fetch(`http://localhost:4000/api/user/removeTodo/${id}`, {
+      method: "delete",
+      headers: { "Content-Type": "application/json", auth: `Bearer ${token}` },
+    });
+    setTodo({todo: todo.counter++})
   };
 
-  const handleEdit = () => {};
   //Returning the Card Component and passing necessary data
   return todos.map((item, i) => {
     return (
       <Card
         title={todos[i].title}
         description={todos[i].description}
+        toBeDoneAt={todos[i].toBeDoneAt}
         key={todos[i]._id}
         _id={todos[i]._id}
         className="mt-2"
-        handleDelete = {()=>handleDelete(todos[i]._id)}
+        handleDelete={() => handleDelete(todos[i]._id)}
         todo={todo}
         setTodo={setTodo}
       />
